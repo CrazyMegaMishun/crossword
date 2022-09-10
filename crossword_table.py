b78223ee613b4d0b1a72e2bdcbdff33b0a2caf6f
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-09-10 20:05:39 +0300

    написал создание таблички

--- crossword_table.py
+++ crossword_table.py
@@ -1,2 +1,28 @@
 """отрисовать матрицу 5х5 с координатами в каждой клетке
     потом наполнить словами"""
+
+from tabulate import tabulate
+from сell import Cell
+
+
+class CrosswordTable:
+    def __init__(self):
+        self.table = self.create_table()
+
+    def create_table(self):
+        table = []
+        for x in range(1, 6):
+            for y in range(1, 6):
+                table.append(Cell(x, y, '_'))
+        return table
+
+    def visualize_table(self, table):
+        
+
+    def word_fill(self):
+        pass
+
+
+if __name__ == '__main__':
+    t = CrosswordTable()
+    print(t.visualize_table(t.table))
--- сell.py
+++ сell.py
@@ -1,11 +1,11 @@
 class Cell:
     def __init__(self, x_coordinate, y_coordinate, letter):
         self.x_coordinate = x_coordinate
-        self.Y_coordinate = y_coordinate
+        self.y_coordinate = y_coordinate
         self.letter = letter
 
     def __str__(self):
-        return f'{self.letter}'
+        return f'{self.x_coordinate, self.y_coordinate}'
 
     def __repr__(self):
         return self.__str__()
